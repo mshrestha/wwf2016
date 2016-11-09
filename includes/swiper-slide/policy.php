e2b7335c78496c1e65e4cb235c27e939208f8b46
rohit <rohitmalakar@gmail.com>
2016-11-09 16:00:54 +0545

    icons strock increased.

--- includes/swiper-slide/policy.php
+++ includes/swiper-slide/policy.php
@@ -4,7 +4,7 @@
 		                <div class="title" data-swiper-parallax="-100">POLICY</div>
 		                <div class="box-wrap rm-col-padding">
 			                <div class="col-sm-12 box-g-ui-2 full-height-child" data-swiper-parallax="-300">
-			                	<div class="col-sm-2 box-g ">
+			                	<div class="col-sm-6 box-g ">
 									<div class="box-container">
 				                		<a href="#" class="hover-overlay dark-purple popup-trigger" id="wwf-nepal">
 					                		<div class="col-sm-12 box box-1 box-title curve-bg dark-purple-box wwf-nepal-title">
@@ -13,17 +13,7 @@
 				                		</a>
 		                			</div>
 			                	</div><!-- /.col-sm-9 -->
-
-			                	<div class="col-sm-5 box-g ">
-									<div class="box-container">
-				                		<a href="#" class="hover-overlay dark-purple popup-trigger" id="policy1">
-					                		<div class="col-sm-12 box box-1 box-title" style="background-image: url('img/policy/policy-img1.jpg');">
-					                			<div class="inner-title">Policy Title</div>
-			                				</div>
-				                		</a>
-		                			</div>
-			                	</div><!-- /.col-sm-9 -->
-			                	<div class="col-sm-5">
+			                	<div class="col-sm-6">
 									<div class="box-container">
 				                		<a href="#" class="hover-overlay dark-purple popup-trigger" id="policy2">
 					                		<div class="col-sm-12 box box-2 box-title" style="background-image: url('img/policy/policy-img2.jpg');">
