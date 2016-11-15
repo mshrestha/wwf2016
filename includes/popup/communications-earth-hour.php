ee4076993f2a3a94e2d71eaf931f6c8160522f65
rohit <rohitmalakar@gmail.com>
2016-11-15 16:52:09 +0545

    max height removed from popup.

--- includes/popup/communications-earth-hour.php
+++ includes/popup/communications-earth-hour.php
@@ -1,6 +1,6 @@
 <img src="img/communications/earth-hour.jpg" alt="" class="hidden">
     	<div id="communications-earth-hour-popup" class="article-section" style="background-image: url('img/communications/earth-hour.jpg'); min-height: 100%; width: 100%; top: 0; left: 0; z-index: 999;">
-            <div class="section-intro content-txt video-content rm-bg" style="overflow: hidden; margin-top: 100px; max-width: 100%;">
+            <div class="section-intro content-txt video-content rm-bg" style="overflow: hidden; margin-top: 100px; max-width: 100%; min-height: 100%;">
             <div class="row">
             	<div class="col-md-4">
 	            	<div class="video-dis">
