bc55af04b70decf092908c9fe8255e2f741cb2bf
Nikesh Ulak <nikesh@kazistudios.com>
2016-11-25 16:29:34 +0545

    Forest popup.

--- includes/popup/forests-habitat-management.php
+++ includes/popup/forests-habitat-management.php
@@ -1,7 +1,13 @@
 		<img src="img/forests/habitat-management-in-protected-areas<?php echo get_small_image();?>.jpg" class="hidden">
-    	<div id="forests-habitat-management-popup" class="article-section" style="background-image: url('img/forests/habitat-management-in-protected-areas<?php echo get_small_image();?>.jpg'); height: 100%; width: 100%; top: 0; left: 0; z-index: 999;">
+    	<div id="forests-habitat-management-popup" class="popup-wrap article-section" style="background-image: url('img/forests/habitat-management-in-protected-areas<?php echo get_small_image();?>.jpg'); height: 100%; width: 100%; top: 0; left: 0; z-index: 999;">
     		<?php copyright('WWF Nepal/Akash Shrestha');?>
-            <div class="section-intro content-txt section-absolute section-right">
+	        <?php
+	        // var_dump($wildlife_section);
+	        $popup_forests_section_current = 0;
+
+	        popup_navigation($popup_forests_section[$popup_forests_section_first], $popup_forests_section[$popup_forests_section_current+1]);
+	        ?>
+            <div class="section-intro content-txt section-absolute section-right box orange-box">
                 <h1 class="uppercase">Habitat management in Protected Areas</h1>
                 <p>Habitat management in TAL is a crucial aspect of protected areas management where park authorities, Nepal Army and local communities undertake grassland management, fire-line construction and maintenance, and waterhole construction and maintenance in order to provide suitable habitat for wildlife all year round. This helps maintain the diversity of palatable grass species, and manage appropriate feeding, drinking, nesting and breeding grounds for wild animals. Seasonal elements such as the monsoons and dry periods have a bearing on the quality of habitat which, if not managed on a regular basis, can be detrimental to the growth and maintenance of wildlife populations.</p>
             </div>
--- includes/popup/forests-main.php
+++ includes/popup/forests-main.php
@@ -1,16 +1,8 @@
-<div id="forests-main-popup" class="popup-wrap scrollbar-outer-div" style="position: fixed; height: 100%; width: 100%;top: 0; left: 0; z-index: 999;">	
-    <div class="scrollbar-inner-div">
-        <div class="scrollbar-div">
-		  	<?php
-		    popup_navigation();
-		    ?>
+
 
 			<?php include('includes/popup/forests-habitat-management.php');?>
 			<?php include('includes/popup/forests-corridors-management.php');?>
-			<?php include('includes/popup/forests-corridors-restoration.php');?>
+			<?php //include('includes/popup/forests-corridors-restoration.php');?>
 			<?php include('includes/popup/forests-sustainable-communities.php');?>
 			<?php include('includes/popup/forests-grazing-control.php');?>
 			<?php //include('includes/popup/forests-green-recovery.php');?>
-		</div>
-	</div>
-</div>
