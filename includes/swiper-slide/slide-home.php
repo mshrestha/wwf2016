6d7724662fc0a2708ef3cd666c94f42a27aabb12
Rohit Malakar <rohit@kazistudios.com>
2016-11-26 17:54:31 +0545

    remove video.

--- includes/swiper-slide/slide-home.php
+++ includes/swiper-slide/slide-home.php
@@ -1,8 +1,5 @@
 				<div class="swiper-slide slide-with-video" data-hash="slide-home">
-					<div class="welcome-video img-cover" style="background-image: url('img/cover.jpg');">
-						<div id="bgndVideo" class="player" data-property="{videoURL:'http://youtu.be/1tWYm9EiwxA',containment:'.welcome-video', autoPlay:false, mute:false, startAt:0, opacity:1, quality: 'medium'}"></div>
-						<!-- , quality:'hd720' -->
-					</div>
+					
 	        		<div class="container-fluid fluid-fixed">
 		                <div class="text intro-text" data-swiper-parallax="-300">
 		                	<div class="box-title">
