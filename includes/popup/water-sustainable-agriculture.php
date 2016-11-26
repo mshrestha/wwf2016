43a43825aab711eb28c699866a5cb8189c5c618f
Nikesh Ulak <nikesh@kazistudios.com>
2016-11-26 16:22:24 +0545

    Remaining Video in wildlife and water.

--- includes/popup/water-sustainable-agriculture.php
+++ includes/popup/water-sustainable-agriculture.php
@@ -1,4 +1,4 @@
-    	<div id="water-sustainable-agriculture-popup" class="article-section popup-wrap" style="background-image: url('img/water/improved-irrigation-opportunities.jpg'); height: 100%; width: 100%; top: 0; left: 0; z-index: 999;">
+    	<div id="water-sustainable-agriculture-popup" class="popup-wrap" style="background-image: url('img/water/improved-irrigation-opportunities.jpg'); height: 100%; width: 100%; top: 0; left: 0; z-index: 999;">
     		<?php copyright('WWF Nepal/Simrika Sharma');?>
 	        <?php
 	        // var_dump($wildlife_section);
@@ -6,6 +6,7 @@
 
 	        popup_navigation($popup_water_section[$popup_water_section_last], $popup_water_section[$popup_water_section_current+1]);
 	        ?>
+	        <div class="article-section">
             <div class="section-intro content-txt box blue-box section-absolute section-right">
             	<h1 class="uppercase">Sustainable agriculture</h1>
 				<p>Together with improving water availability for irrigation purposes, a key focus of the Integrated Water Resource Management (IWRM) project  in the Indrawati and Dudhkoshi sub-basins, is on providing facilities and building capacity of local communities with regard to climate-smart and sustainable agriculture. Conservation ponds were constructed to collect rainwater and run-off water as a storage to be used primarily during the dry seasons for vegetable farming while irrigation canals were maintained to provide access to water for irrigation purposes. Through the construction of poly-houses, local people no longer had to forego harvests on account of unpredictable weather patterns that damaged crops. The project also organized farmers’ schools on integrated pest management with the objective of reducing the use of chemical fertilizers and pesticides, and promoting organic farming. These interventions have helped promote sustainable agriculture and enhanced the livelihoods of local communities in the sub-basins.</p>
@@ -71,4 +72,19 @@
 					</div>
 				</div>
         	</div>
+        	</div>
+
+			<div class="article-section video-section-full box blue-box" style="">
+			    <div class="section-intro content-txt video-content rm-bg" style="overflow: hidden; margin-top: 70px; max-width: 100%;">
+			        <div class="row">
+			        	<div class="col-xs-8 col-xs-offset-2">
+				            <div class="video-sec">
+				                <div class="embed-responsive embed-responsive-16by9">
+								    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/VZGygp2dMR8"></iframe>
+								</div>
+							</div>
+			        	</div>
+			        </div>
+			    </div> <!-- /.section-intro -->
+			</div>
     	</div>
--- includes/popup/wildlife-zero-poaching.php
+++ includes/popup/wildlife-zero-poaching.php
@@ -1,5 +1,5 @@
       <img src="img/wildlife/zero-poaching-image<?php echo get_small_image();?>.jpg" class="hidden">
-    	<div id="wildlife-zero-poaching-popup" class="popup-wrap article-section" style="background-image: url('img/wildlife/zero-poaching-image<?php echo get_small_image();?>.jpg'); height: 100%; width: 100%;position: fixed; top: 0; left: 0; z-index: 999;">
+    	<div id="wildlife-zero-poaching-popup" class="popup-wrap" style="background-image: url('img/wildlife/zero-poaching-image<?php echo get_small_image();?>.jpg'); height: 100%; width: 100%;position: fixed; top: 0; left: 0; z-index: 999;">
         <?php copyright('WWF-US/Narendra Shrestha');?>
         <?php
         // var_dump($wildlife_section);
@@ -7,6 +7,7 @@
 
         popup_navigation($popup_wildlife_section[$popup_wildlife_section_current-1], $popup_wildlife_section[$popup_wildlife_section_current+1]);
         ?>
+        <div class="article-section">
         <div class="infographics">
           <div id="infographics-zero-poaching" class="infographics-widget clearfix">
             <div class="row">
@@ -79,6 +80,22 @@
           <h1 class="uppercase">THE ZERO POACHING STANDARD</h1>
           <p>On 2 May 2016, Nepal marked two years since its last rhino was poached. This is the first time that Nepal achieved two consecutive years of zero poaching of rhinos whose population presently stands at 645, the highest recorded number in the country so far. Nepal’s zero poaching success is rooted in a coordinated response, which is driven from the centre and implemented at grassroots level, including heightened protection efforts within Protected Areas and buffer zones and community forests. Nepal has also been a driving force behind the Zero Poaching Toolkit, which highlights the six pillars that countries need to focus on to tackle poaching.</p>
         </div> <!-- /.section-intro -->
+        </div>
+
+
+        <div class="article-section video-section-full">
+            <div class="section-intro content-txt video-content rm-bg" style="overflow: hidden; margin-top: 70px; max-width: 100%;">
+                <div class="row">
+                  <div class="col-xs-8 col-xs-offset-2">
+                      <div class="video-sec">
+                          <div class="embed-responsive embed-responsive-16by9">
+                      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/gF2wBMGLTA0"></iframe>
+                  </div>
+                </div>
+                  </div>
+                </div>
+            </div> <!-- /.section-intro -->
+        </div>
     	</div>
       <style>
         #wildlife-zero-poaching-popup .infographics{
