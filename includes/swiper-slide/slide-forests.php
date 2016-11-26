30e75740e71b931932183013f827584c92e84f8b
Nikesh Ulak <nikesh@kazistudios.com>
2016-11-26 16:24:46 +0545

    Forest popup link fixed.

--- includes/swiper-slide/slide-forests.php
+++ includes/swiper-slide/slide-forests.php
@@ -18,7 +18,7 @@
 			                	<div class="col-sm-6 box-g box-g-ui-5" data-swiper-parallax="-200">
 			                		<div class="col-sm-12">
 			                			<div class="box-container">
-					                		<a href="forests-corridors-restoration-popup" class="hover-overlay orange popup-trigger" id="forests-main">
+					                		<a href="forests-corridors-management-popup" class="hover-overlay orange popup-trigger" id="forests-main">
 						                		<div class="col-sm-12 box-title box box-1" style="background-image: url('img/forests/forest-management-in-critical-corridors-600.jpg');">
 						                			<div class="inner-title center">FOREST RESTORATION AND MANAGEMENT IN CRITICAL CORRIDORS</div>
 						                		</div>
