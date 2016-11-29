5b781b69a0b0d70d4bd7c662426f7b447ed5ff00
Nikesh Ulak <nikesh@kazistudios.com>
2016-11-29 14:38:36 +0545

    Removed next and previous link in last and first popup.

--- includes/functions.php
+++ includes/functions.php
@@ -29,9 +29,30 @@ function popup_navigation($previous_story_id = '', $next_story_id = '') {
 	// echo '<div class="loading"></div>';
   echo '<ul class="popup-navigation">';
     echo '<li class="popup-close"><span class="icon-cross" title="Click or Press ESC Key"></span></li>';
-    if($previous_story_id != '' && $next_story_id != '') {
-    echo '<li><a href="'.$previous_story_id.'-popup" class="popup-trigger previous-story" id="'.$previous_story_id.'">Previous Story</a></li>
-          <li><a href="'.$next_story_id.'-popup" class="popup-trigger next-story" id="'.$next_story_id.'">Next Story</a></li>';
+    if($previous_story_id != '' && $next_story_id != '') {   
+
+      $all_last_story_id = ['wildlife-reducing-human-wildlife-conflict', 'forests-grazing-control', 'climate-change-permanent', 'water-piloting-payments', 'policy-sustainable', 'communications-tgg'];
+      // print_r($previous_story_id);
+      if( in_array($previous_story_id, $all_last_story_id) ) {
+        //i.e prev story is the last story, hide prev story link
+        $hidePrevStory = 'hidden';
+      }
+      else {
+        $hidePrevStory = '';
+      }
+
+      $all_first_story_id = ['wildlife-rhino-translocation', 'forests-habitat-management', 'climate-change-participation', 'water-sustainable-agriculture', 'policy-tal-nepal', 'communications-prince-harry'];
+      // print_r($next_story_id);
+      if( in_array($next_story_id, $all_first_story_id) ) {
+        //i.e next story is the first story, hide next story link
+        $hideNextStory = 'hidden';
+      }
+      else {
+        $hideNextStory = '';
+      }
+
+      echo '<li class="'.$hidePrevStory.'"><a href="'.$previous_story_id.'-popup" class="popup-trigger previous-story" id="'.$previous_story_id.'">Previous Story</a></li>';
+      echo '<li class="'.$hideNextStory.'"><a href="'.$next_story_id.'-popup" class="popup-trigger next-story" id="'.$next_story_id.'">Next Story</a></li>';
     }//end if
   echo '</ul>';
 }
